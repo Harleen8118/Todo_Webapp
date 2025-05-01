86560a1afe7465b025df6605a2d27e396252c61a
Harleen8118 <harleenbbagga@gmail.com>
2025-05-01 20:45:20 +0530

    update

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ listsContainer.addEventListener('click', e => {
     saveAndRender()
   }
 })
+
+tasksContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+    selectedTask.complete = e.target.checked
+    save()
+    renderTaskCount(selectedList)
+  }
+})
